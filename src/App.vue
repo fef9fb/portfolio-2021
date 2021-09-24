76eef0122db0d6bcc8fd00e2a1915f7423099476
Lisa Ino <lisaino@LisanoMacBook.local>
2021-09-24 17:03:55 +0900

    create main style

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,24 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <h1 class="display-2 font-weight-bold">
+        人物
+      </h1>
+      <Person/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import Person from './components/Person';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Person,
   },
 
   data: () => ({
@@ -61,7 +66,19 @@ export default {
 
 <style scoped lang="scss">
 
+h1 {
+  position: fixed;
+  width: 1200px;
+  top: 64px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
 .theme--light.v-app-bar.v-toolbar.v-sheet {
   background-color: rgba(255, 255, 255, 0);
+}
+
+.v-main {
+  padding: 0 !important;
 }
 </style>
---
+++ src/components/Person.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="container-wrapper bg-1 pt-16">
+      <v-container class="pt-16">
+        <v-row class="text-center">
+
+          <v-col cols="12">
+            <h2 class="mb-3">
+              プロフィール
+            </h2>
+            <div class="col-main">
+              名前
+            </div>
+
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+
+    <div class="container-wrapper bg-2 pt-16">
+      <v-container>
+      </v-container>
+    </div>
+    <div class="container-wrapper bg-3 pt-16">
+      <v-container>
+      </v-container>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Person',
+
+  data: () => ({}),
+}
+</script>
+<style scoped lang="scss">
+@use '../sass/main' as main;
+
+
+</style>
